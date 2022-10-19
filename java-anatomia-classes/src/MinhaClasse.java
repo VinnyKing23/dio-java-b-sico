f71fd45c5509e7577f8db26c0e5fcae41698025f
VinnyKing23 <vinydi@hotmail.com>
2022-10-18 21:51:58 -0300

    minhaclasse

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,12 @@
+public class MinhaClasse {
+    
+    public static void main(String []args) {
+        String meuNome = "Vinicius";
+
+        int anoFabricação = 2002;
+
+        boolean verdadeira = false;
+    }
+        
+    }
+
